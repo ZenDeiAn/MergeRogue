0941dfb63555ae3c322dfb24a01f01237e57a04c
zendeian <zendeian@naver.com>
2025-02-16 23:31:14 +0900

    ==

---
+++ Assets/Work/HotUpdate/Script/CanvasActor.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasActor : MonoBehaviour
+{
+    public Camera targetCamera; // The camera to face; if null, it defaults to the main camera.
+    public Actor actor;
+    [SerializeField] private Slider sld_health;
+    [SerializeField] private Slider sld_skill;
+    
+    public void UpdateCanvas()
+    {
+        sld_health.value = actor.Status.Health / (float)actor.Status.HealthMaximumCalculated;
+    }
+    
+    void Start()
+    {
+        if (targetCamera == null)
+        {
+            targetCamera = Camera.main; // Assign the main camera if no camera is set.
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (targetCamera == null)
+            return;
+
+        // Make the Canvas face the camera
+        transform.LookAt(transform.position + targetCamera.transform.rotation * Vector3.forward,
+            targetCamera.transform.rotation * Vector3.up);
+    }
+
+    public void Initialize(Actor self)
+    {
+        actor = self;
+        UpdateCanvas();
+    }
+}
